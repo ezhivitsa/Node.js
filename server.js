bba47dd1b63b08001819dca990565139189adf82
Evgeny Zhivitsa <j.zhivitsa@gmail.com>
2015-11-04 23:55:58 +0300

    Added functionality to get static files from the server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var http = require('http');
 
 var fileGet = require('./lib/file_get.js'),
-	requestType = require('./lib/requset_type.js'),
+	requestType = require('./lib/request_type.js'),
 	app = require('./lib/app.js');
 
 var server;
 
 server = http.createServer();
-server.on('request', function (request) {
-	if ( requestType(request) === app.requestType.getFile ) {
+server.on('request', function (request, responce) {
+	if ( requestType.getType(request) === app.requestType.getFile ) {
 		// get file from the filesystem
-		fileGet.get();
+		fileGet.get(request, responce);
 	}
 });
 server.listen(9999);
